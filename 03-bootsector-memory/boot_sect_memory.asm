858ed1786844bffb22ce995edf690fefb03b9cbe
keishi kubo <admin@keishis-iMac.local>
2022-11-20 01:52:42 +0900

    a

--- 03-bootsector-memory/boot_sect_memory.asm
+++ 03-bootsector-memory/boot_sect_memory.asm
@@ -30,6 +30,11 @@ mov al, [bx]
 int 0x10
 int 0x10
 
+
+the_secret:
+    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
+    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
+    db "X"
 ; attempt 4
 ; We try a shortcut since we know that the X is stored at byte 0x2d in our binary
 ; That's smart but ineffective, we don't want to be recounting label offsets
@@ -37,18 +42,14 @@ int 0x10
 mov al, "4"
 int 0x10
 int 0x10
-mov al, [0x7c36]
+mov al, [0x7c38]
 int 0x10
-mov al, [0x7c03]
+mov al, [0x7c07]
 int 0x10
 int 0x10
 
 jmp $ ; infinite loop
 
-the_secret:
-    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
-    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
-    db "X"
 
 ; zero padding and magic bios number
 times 510-($-$$) db 0
