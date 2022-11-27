79c20a58e3c21a380f2fdb32665cdf5eec60fc58
keishi kubo <admin@keishis-iMac.local>
2022-11-27 16:11:59 +0900

    add comment

--- 07-bootsector-disk/boot_sect_main.asm
+++ 07-bootsector-disk/boot_sect_main.asm
@@ -2,8 +2,9 @@
     mov bp, 0x8000 ; set the stack safely away from us
     mov sp, bp
 
-    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-    mov dh, 2 ; read 2 sectors
+    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000 (Int13-2の仕様:https://stanislavs.org/helppc/int_13-2.html)。この領域にデータがコピーされる
+    mov dh, 2 ; read 2 sectors // 512から2sector文を、bx 0x9000からはじまるstack領域にコピーする
+    ;  mov dh, 3 ; read 3 sectors // 512から3sector文を、bx 0x9000からはじまるstack領域にコピーする
     ; the bios sets 'dl' for our boot disk number
     ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
     call disk_load
@@ -14,6 +15,9 @@
     call print_nl
 
     mov dx, [0x9000 + 512] ; first word from second loaded sector, 0xface
+    call print_hex
+    call print_nl
+    mov dx, [0x9000 + 1024] ; first word from second loaded sector, 0xface
     call print_hex
 
     jmp $
@@ -29,4 +33,5 @@ dw 0xaa55
 ; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
 ; from now on = sector 2 ...
 times 256 dw 0xdada ; sector 2 = 512 bytes
-times 256 dw 0xface ; sector 3 = 512 bytes
+times 256 dw 0xcdef ; sector 3 = 512 bytes
+times 256 dw 0xface ; sector 4 = 512 bytes
